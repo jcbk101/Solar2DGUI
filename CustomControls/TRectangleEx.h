f792c18286b4bfcdac17690930c45f570924827d
CodeIsLife101 [Bongo`] <75145117+jcbk101@users.noreply.github.com>
2022-09-15 22:48:42 -0400

    Add files via upload

--- CustomControls/TRectangleEx.h
+++ CustomControls/TRectangleEx.h
@@ -41,66 +41,66 @@ public:
 	}
 
 
-	//Setters
-	void setScaleX(float X)
-	{
-		Scale->X = X;
-	}
-
-
-	void setScaleY(float Y)
-	{
-		Scale->Y = Y;
-	}
-
-
-	void setRotationAngle(float Angle)
-	{
-		RotationAngle = Angle;
-	}
-
-
-	void setRotationCenterX(float X)
-	{
-		RotationCenter->X = X;
-	}
-
-
-	void setRotationCenterY(float Y)
-	{
-		RotationCenter->Y = Y;
-	}
-
-
-	//Getters
-	float getScaleX()
-	{
-		return Scale->X;
-	}
-
-
-	float getScaleY()
-	{
-		return Scale->Y;
-	}
-
-
-	float getRotationAngle()
-	{
-		return RotationAngle;
-	}
-
-
-	float getRotationCenterX()
-	{
-		return RotationCenter->X;
-	}
-
-
-	float getRotationCenterY()
-	{
-		return RotationCenter->Y;
-	}
+//	//Setters
+//	void setScaleX(float X)
+//	{
+//		Scale->X = X;
+//	}
+//
+//
+//	void setScaleY(float Y)
+//	{
+//		Scale->Y = Y;
+//	}
+//
+//
+//	void setRotationAngle(float Angle)
+//	{
+//		RotationAngle = Angle;
+//	}
+//
+//
+//	void setRotationCenterX(float X)
+//	{
+//		RotationCenter->X = X;
+//	}
+//
+//
+//	void setRotationCenterY(float Y)
+//	{
+//		RotationCenter->Y = Y;
+//	}
+//
+//
+//	//Getters
+//	float getScaleX()
+//	{
+//		return Scale->X;
+//	}
+//
+//
+//	float getScaleY()
+//	{
+//		return Scale->Y;
+//	}
+//
+//
+//	float getRotationAngle()
+//	{
+//		return RotationAngle;
+//	}
+//
+//
+//	float getRotationCenterX()
+//	{
+//		return RotationCenter->X;
+//	}
+//
+//
+//	float getRotationCenterY()
+//	{
+//		return RotationCenter->Y;
+//	}
 
 	enum
 	{
